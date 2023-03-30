0172a9b213bd1a71862280f9f0371f250c19ea83
Subinacls <admin@hacker-island.com>
2023-03-30 12:10:40 -0400

    Update view_configmap.sh

--- bash/openshift/api-resources/view_configmap.sh
+++ bash/openshift/api-resources/view_configmap.sh
@@ -1,9 +1,24 @@
 """
 This function takes two parameters:
 
-namespace (optional): the name of the OpenShift namespace to view configmaps in. If not provided, the current project will be used.
-configmap (optional): the name of the specific configmap to view. If not provided, the user will be prompted to select one.
-The function first checks if a specific configmap was provided. If not, it prompts the user to select one from a list of all configmaps in the specified namespace. Once a configmap has been selected, the function retrieves the configmap's data in JSON format using oc get configmap and pipes the output to jq to extract and display only the .data section.
+namespace (optional): 
+  the name of the OpenShift namespace to view configmaps in. 
+  If not provided,
+    the current project will be used.
+configmap (optional): 
+  the name of the specific configmap to view.
+  If not provided,
+    the user will be prompted to select one.
+
+The function first checks if a specific configmap was provided. 
+  If not, 
+    it prompts the user to select one from a list of all configmaps in the specified namespace.
+  Once a configmap has been selected,
+    the function retrieves the configmap's data in JSON format using
+        oc get configmap
+        pipes the output to jq
+        extracts the contents from the data section.
+  Displays the results to the user through STDOUT
 
 You can use this function by calling it with the appropriate parameters, like this:
 
@@ -27,7 +42,6 @@ EXAMPLE OUTPUT:
 function view_configmap() {
   local namespace=${1:-$(oc project -q)}
   local configmap=$2
-
   if [ -z "$configmap" ]; then
     # If no configmap was provided, prompt the user to select one
     echo "No configmap provided. Please select one from the list below:"
@@ -39,6 +53,5 @@ function view_configmap() {
       return 1
     fi
   fi
-
   oc get configmap $configmap -n $namespace -o json | jq '.data | .. | . ' | xargs -0 echo -e;
 }; # view_configmap myproject myconfigmap
