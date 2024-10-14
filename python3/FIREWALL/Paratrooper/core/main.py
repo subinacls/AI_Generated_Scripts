c121266d3061ffb0293f734cbb00a91af295d46e
Subinacls <admin@hacker-island.com>
2024-10-13 21:57:06 -0400

    Update main.py

--- python3/FIREWALL/Paratrooper/core/main.py
+++ python3/FIREWALL/Paratrooper/core/main.py
@@ -1,5 +1,11 @@
 import sys
 
+# Ensuring input() works across both versions
+try:
+    input = raw_input  # In Python 2, raw_input() is used
+except NameError:
+    pass  # In Python 3, input() is already correct
+
 # Ensure compatibility between Python 2 and 3 for builtins
 if sys.version_info[0] < 3:
     import __builtin__ as bi  # Python 2
