27be78925c44cfac938622ad14d189b1795019be
Subinacls <admin@hacker-island.com>
2024-10-13 21:45:30 -0400

    Create banner.py

---
+++ python3/FIREWALL/Paratrooper/core/banner.py
@@ -0,0 +1,9 @@
+
+from os import popen as popen
+def banner():
+    popen('reset; clear')
+    print '\n\t+' + '-' * 31 + '+'
+    print '\t| SecretNOC, LLC - 2024\t|'
+    print '\t| William (SubINaclS) Coppola\t|'
+    print '\t| Codename: Paratrooper\t\t|'
+    print '\t+' + '-' * 31 + '+\n'
