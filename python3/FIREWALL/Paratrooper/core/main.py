1ebb81f9c0b0f6a65669f979ae639684c55969a4
Subinacls <admin@hacker-island.com>
2024-10-13 21:56:40 -0400

    Create main.py

---
+++ python3/FIREWALL/Paratrooper/core/main.py
@@ -0,0 +1,10 @@
+import sys
+
+# Ensure compatibility between Python 2 and 3 for builtins
+if sys.version_info[0] < 3:
+    import __builtin__ as bi  # Python 2
+else:
+    import builtins as bi     # Python 3
+
+##### SET DEBUG #####
+bi.diag = True  # If True, diagnostics are enabled
